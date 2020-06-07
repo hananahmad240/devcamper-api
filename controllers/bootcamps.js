a2cb16d8e4831ce6bde9b2093cbae002f901700b
hanan ahmad <ahmadhanan344@gmail.com>
2020-06-07 10:11:36 -0700

    third commit

---
+++ controllers/bootcamps.js
@@ -0,0 +1,60 @@
+// @desc       get all bootcamps
+// @route      GET /api/v1/bootcamps
+// @access     public
+
+exports.getAllBootcamps = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        msg: "All bootcamps"
+    })
+}
+
+
+
+// @desc       get single bootcamps
+// @route      GET /api/v1/bootcamps/:id
+// @access     public
+
+exports.getSingBootcamps = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        msg: `single botcamps with id ${req.params.id}`
+    })
+}
+
+
+
+// @desc       add new bootcamps
+// @route      POST /api/v1/bootcamps
+// @access     public
+
+exports.addBootcamps = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        msg: `add new bootcamp`
+    });
+}
+
+
+// @desc       update single bootcamps
+// @route      PUT /api/v1/bootcamps/:id
+// @access     public
+
+exports.updateBootcamps = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        msg: `update botcamps with id ${req.params.id}`
+    })
+}
+
+
+// @desc       delete single bootcamps
+// @route      DELETE /api/v1/bootcamps/:id
+// @access     public
+
+exports.deleteBootcamps = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        msg: ` delete single botcamps with id ${req.params.id}`
+    })
+}
--- routes/bootcamp.js
+++ routes/bootcamp.js
@@ -1,49 +1,67 @@
 const express = require('express');
+const {
+    getAllBootcamps,
+    getSingBootcamps,
+    updateBootcamps,
+    deleteBootcamps,
+    addBootcamps
+} = require('../controllers/bootcamps');
+
+
+// creat route
 const router = express.Router();
 
-
-// get all bootcamp
-router.get('/bootcamps', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: "All bootcamps"
-    })
-});
+router.route('/bootcamps').get(getAllBootcamps); //get all
+router.route('/bootcamps/:id').get(getSingBootcamps); // get single
+router.route('/bootcamps/:id').put(updateBootcamps); // update single
+router.route('/bootcamps/:id').delete(deleteBootcamps); // delete single
+router.route('/bootcamps').post(addBootcamps); // add new
 
 
-// get single bootcamp
-router.get('/bootcamps/:id', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: `single botcamps with id ${req.params.id}`
-    })
-});
 
 
-// add new bootcamp bootcamp
-router.post('/bootcamps', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: `add new bootcamp`
-    })
-});
+// // get all bootcamp
+// router.get('/bootcamps', (req, res) => {
+//     res.status(200).json({
+//         success: true,
+//         msg: "All bootcamps"
+//     })
+// });
 
 
-// update single bootcampbootcamp
-router.put('/bootcamps/:id', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: `update botcamps with id ${req.params.id}`
-    })
-});
+// // get single bootcamp
+// router.get('/bootcamps/:id', (req, res) => {
+//     res.status(200).json({
+//         success: true,
+//         msg: `single botcamps with id ${req.params.id}`
+//     })
+// });
 
-// delete single bootcamp
-router.delete('/bootcamps/:id', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: ` delete single botcamps with id ${req.params.id}`
-    })
-});
+
+// // add new bootcamp bootcamp
+// router.post('/bootcamps', (req, res) => {
+//     res.status(200).json({
+//         success: true,
+//         msg: `add new bootcamp`
+//     })
+// });
+
+
+// // update single bootcampbootcamp
+// router.put('/bootcamps/:id', (req, res) => {
+//     res.status(200).json({
+//         success: true,
+//         msg: `update botcamps with id ${req.params.id}`
+//     })
+// });
+
+// // delete single bootcamp
+// router.delete('/bootcamps/:id', (req, res) => {
+//     res.status(200).json({
+//         success: true,
+//         msg: ` delete single botcamps with id ${req.params.id}`
+//     })
+// });
 
 
 // export
